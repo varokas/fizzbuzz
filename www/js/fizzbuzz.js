15250e54ca7d6e4dd465e5d7b7ddfd9d206d1b15
Varokas Panusuwan <varokas@panusuwan.com>
2014-11-25 15:51:19 +0700

    mod 5

--- www/js/fizzbuzz.js
+++ www/js/fizzbuzz.js
@@ -1,7 +1,7 @@
 function toFizzBuzz(number) {
   if(number == 15) return 'FizzBuzz';
   if(number % 3 == 0) return 'Fizz';
-  if(number % 5 == 0) return 'Buzz';
+  //if(number % 5 == 0) return 'Buzz';
 
   return number;
 }
